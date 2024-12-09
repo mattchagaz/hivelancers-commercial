0f5b7cab24ab8dc2c36cd9c4e91f331908af15af
Matheus Chagas <matheus.chagas159700@gmail.com>
2024-12-09 19:19:06 -0300

    updates

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,10 +2,10 @@ import styles from './hero.module.css'
 import { FaSearch } from "react-icons/fa";
 import { FiFileText } from "react-icons/fi";
 import HivelancersImage from '../../assets/Screenshot 2024-12-09 at 16.13.30.png'
-import HivelancersImage2 from '../../assets/Screenshot 2024-12-09 at 16.14.08.png'
 
 export default function Hero() {
   return (
+
     <div className={styles.container}>
       <div className={styles.content}>
         <h1 className={styles.title}>
@@ -28,7 +28,7 @@ export default function Hero() {
             <span className={styles.browserDot}></span>
           </div>
           <div className={styles.browserAddressBar}>
-            <p className={styles.browserAddressContent}>hivelancers.com</p>
+            <p className={styles.browserAddressContent}>www.hivelancers.com</p>
           </div>
           <div className={styles.browserActions}>
             <span className={styles.browserAction}></span>
