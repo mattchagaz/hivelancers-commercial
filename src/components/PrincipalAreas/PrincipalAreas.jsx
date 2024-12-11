4827db90ebd604bcb2278d0998c41ca086abf6ab
Matheus Chagas <matheus.chagas159700@gmail.com>
2024-12-11 16:10:31 -0300

    updates

--- src/components/PrincipalAreas/PrincipalAreas.jsx
+++ src/components/PrincipalAreas/PrincipalAreas.jsx
@@ -6,153 +6,153 @@ import styles from './PrincipalAreas.module.css';
 
 export default function PrincipalAreas() {
   return (
-    <>
     <div className={styles.Container}>
-    <h1 className={styles.label}>principais áreas</h1>
-    <p className={styles.description}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua consectetur adipiscing elit, sed do eiusmod tempor.</p>
-    <div className={styles.carouselContainer}>
-      {/* Primeiro carrossel */}
-      <Splide
-        options={{
-          type: 'loop',
-          drag: 'free',
-          autoWidth: true,
-          focus: 'center',
-          perPage: 3, // Número de slides visíveis
-          autoScroll: {
-            speed: 1, // Velocidade do auto-scroll (ajuste conforme necessário)
-          },
-          arrows: false,
-          pagination: false,
-        }}
-        extensions={{ AutoScroll }}
-        className={styles.carouselSpacing}
-      >
-        <SplideSlide>
-          <div className={styles.slide}>Contabilidade</div>
-        </SplideSlide>
-        <SplideSlide>
-          <div className={styles.slide}>Desenvolvimento Mobile</div>
-        </SplideSlide>
-        <SplideSlide>
-          <div className={styles.slide}>Marketing</div>
-        </SplideSlide>
-        <SplideSlide>
-          <div className={styles.slide}>UI Design</div>
-        </SplideSlide>
-        <SplideSlide>
-          <div className={styles.slide}>Desenvolvimento Web</div>
-        </SplideSlide>
-        <SplideSlide>
-          <div className={styles.slide}>Design Gráfico</div>
-        </SplideSlide>
-        <SplideSlide>
-          <div className={styles.slide}>UX Design</div>
-        </SplideSlide>
-        <SplideSlide>
-          <div className={styles.slide}>Ilustração</div>
-        </SplideSlide>
-        <SplideSlide>
-          <div className={styles.slide}>Redação</div>
-        </SplideSlide>
-      </Splide>
+      <h1 className={styles.label}>principais áreas</h1>
+      <p className={styles.description}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua consectetur adipiscing elit, sed do eiusmod tempor.</p>
+      <div className={styles.carouselContainer}>
 
-      {/* Segundo carrossel */}
-      <Splide
-        options={{
-          type: 'loop',
-          drag: 'free',
-          autoWidth: true,
-          focus: 'center',
-          perPage: 3,
-          autoScroll: {
-            speed: 1,
-          },
-          arrows: false,
-          pagination: false,
-        }}
-        extensions={{ AutoScroll }}
-        className={styles.carouselSpacing} 
-      >
-        <SplideSlide>
-          <div className={styles.slide}>Desenvolvimento Mobile</div>
-        </SplideSlide>
-        <SplideSlide>
-          <div className={styles.slide}>Contabilidade</div>
-        </SplideSlide>
-        <SplideSlide>
-          <div className={styles.slide}>UI Design</div>
-        </SplideSlide>
-        <SplideSlide>
-          <div className={styles.slide}>Desenvolvimento Web</div>
-        </SplideSlide>
-        <SplideSlide>
-          <div className={styles.slide}>Design Gráfico</div>
-        </SplideSlide>
-        <SplideSlide>
-          <div className={styles.slide}>UX Design</div>
-        </SplideSlide>
-        <SplideSlide>
-          <div className={styles.slide}>Ilustração</div>
-        </SplideSlide>
-        <SplideSlide>
-          <div className={styles.slide}>Redação</div>
-        </SplideSlide>
-        <SplideSlide>
-          <div className={styles.slide}>Marketing</div>
-        </SplideSlide>
-      </Splide>
+        <Splide
+          options={{
+            type: 'loop',
+            drag: 'free',
+            autoWidth: true,
+            focus: 'center',
+            perPage: 3,
+            autoScroll: {
+              speed: 1,
+              direction: 'ltr', 
+            },
+            arrows: false,
+            pagination: false,
+          }}
+          extensions={{ AutoScroll }}
+          className={styles.carouselSpacing}
+        >
+          <SplideSlide>
+            <div className={styles.slide}>UI Design</div>
+          </SplideSlide>
+          <SplideSlide>
+            <div className={styles.slide}>Marketing Digital</div>
+          </SplideSlide>
+          <SplideSlide>
+            <div className={styles.slide}>Produção e Edição de Vídeo</div>
+          </SplideSlide>
+          <SplideSlide>
+            <div className={styles.slide}>Desenvolvimento Web</div>
+          </SplideSlide>
+          <SplideSlide>
+            <div className={styles.slide}>SEO</div>
+          </SplideSlide>
+          <SplideSlide>
+            <div className={styles.slide}>Tradução e Localização</div>
+          </SplideSlide>
+          <SplideSlide>
+            <div className={styles.slide}>Consultoria Empresarial</div>
+          </SplideSlide>
+          <SplideSlide>
+            <div className={styles.slide}>Redação de Artigos e Blogs</div>
+          </SplideSlide>
+          <SplideSlide>
+            <div className={styles.slide}>Social Media Management</div>
+          </SplideSlide>
+        </Splide>
 
-      {/* Terceiro carrossel */}
-      <Splide
-        options={{
-          type: 'loop',
-          drag: 'free',
-          autoWidth: true,
-          focus: 'center',
-          perPage: 3,
-          autoScroll: {
-            speed: 1,
-          },
-          arrows: false,
-          pagination: false,
-        }}
-        extensions={{ AutoScroll }}
-        className={styles.carouselSpacing}
-      >
-        <SplideSlide>
-          <div className={styles.slide}>Redação</div>
-        </SplideSlide>
-        <SplideSlide>
-          <div className={styles.slide}>Ilustração</div>
-        </SplideSlide>
-        <SplideSlide>
-          <div className={styles.slide}>UI Design</div>
-        </SplideSlide>
-        <SplideSlide>
-          <div className={styles.slide}>Design Gráfico</div>
-        </SplideSlide>
-        <SplideSlide>
-          <div className={styles.slide}>UX Design</div>
-        </SplideSlide>
-        <SplideSlide>
-          <div className={styles.slide}>Desenvolvimento Web</div>
-        </SplideSlide>
-        <SplideSlide>
-          <div className={styles.slide}>UX Design</div>
-        </SplideSlide>
-        <SplideSlide>
-          <div className={styles.slide}>Marketing</div>
-        </SplideSlide>
-        <SplideSlide>
-          <div className={styles.slide}>Desenvolvimento Mobile</div>
-        </SplideSlide>
-        <SplideSlide>
-          <div className={styles.slide}>Contabilidade</div>
-        </SplideSlide>
-      </Splide>
+        <Splide
+          options={{
+            type: 'loop',
+            drag: 'free',
+            autoWidth: true,
+            focus: 'center',
+            perPage: 3,
+            autoScroll: {
+              speed: -1,
+              direction: 'rtl',
+            },
+            arrows: false,
+            pagination: false,
+          }}
+          extensions={{ AutoScroll }}
+          className={styles.carouselSpacing}
+        >
+          <SplideSlide>
+            <div className={styles.slide}>Desenvolvimento Mobile</div>
+          </SplideSlide>
+          <SplideSlide>
+            <div className={styles.slide}>Produção e Edição de Áudio</div>
+          </SplideSlide>
+          <SplideSlide>
+            <div className={styles.slide}>Automação e Scripts</div>
+          </SplideSlide>
+          <SplideSlide>
+            <div className={styles.slide}>Copywriting</div>
+          </SplideSlide>
+          <SplideSlide>
+            <div className={styles.slide}>Animação e Motion Graphics</div>
+          </SplideSlide>
+          <SplideSlide>
+            <div className={styles.slide}>Machine Learning e Inteligência Artificial</div>
+          </SplideSlide>
+          <SplideSlide>
+            <div className={styles.slide}>UX Design</div>
+          </SplideSlide>
+          <SplideSlide>
+            <div className={styles.slide}>Fotografia</div>
+          </SplideSlide>
+          <SplideSlide>
+            <div className={styles.slide}>Desenvolvimento de Jogos</div>
+          </SplideSlide>
+        </Splide>
+
+        <Splide
+          options={{
+            type: 'loop',
+            drag: 'free',
+            autoWidth: true,
+            focus: 'center',
+            perPage: 3,
+            autoScroll: {
+              speed: 1,
+              direction: 'ltr', 
+            },
+            arrows: false,
+            pagination: false,
+          }}
+          extensions={{ AutoScroll }}
+          className={styles.carouselSpacing}
+        >
+          <SplideSlide>
+            <div className={styles.slide}>Design de Apresentações</div>
+          </SplideSlide>
+          <SplideSlide>
+            <div className={styles.slide}>Segurança da Informação</div>
+          </SplideSlide>
+          <SplideSlide>
+            <div className={styles.slide}>Aulas Online e Tutoria</div>
+          </SplideSlide>
+          <SplideSlide>
+            <div className={styles.slide}>Desenvolvimento de Software</div>
+          </SplideSlide>
+          <SplideSlide>
+            <div className={styles.slide}>Gestão de Mídias Pagas</div>
+          </SplideSlide>
+          <SplideSlide>
+            <div className={styles.slide}>DevOps</div>
+          </SplideSlide>
+          <SplideSlide>
+            <div className={styles.slide}>Design de Moda e Estilo</div>
+          </SplideSlide>
+          <SplideSlide>
+            <div className={styles.slide}>Análise de Dados</div>
+          </SplideSlide>
+          <SplideSlide>
+            <div className={styles.slide}>Treinamento Corporativo</div>
+          </SplideSlide>
+          <SplideSlide>
+            <div className={styles.slide}>Contabilidade</div>
+          </SplideSlide>
+        </Splide>
+      </div>
     </div>
-    </div>
-    </>
   );
 }
+
