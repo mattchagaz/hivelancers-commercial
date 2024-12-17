f1a33fc36867af12482cfcfaee87958fe1d695ae
Matheus Chagas <matheus.chagas159700@gmail.com>
2024-12-17 13:57:56 -0300

    update navbar mobile

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,35 +1,66 @@
-import React from "react";
-import styles from "./Navbar.module.css"
+import React, { useState, useEffect } from "react";
+import styles from "./Navbar.module.css";
 import { LuArrowUpRight } from "react-icons/lu";
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+import { GiHamburgerMenu } from "react-icons/gi"; 
+import AOS from "aos";
+import "aos/dist/aos.css";
 
-const Navbar = () => { 
-    AOS.init();
+const Navbar = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    useEffect(() => {
+        AOS.init();
+    }, []);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
     return (
         <div className={styles.navbar}>
             <a href="/">
-                <div  className={styles.logo}>Hivelancers</div>
+                <div className={styles.logo}>Hivelancers</div>
             </a>
-            <nav className={styles.nav}>
+            <button className={styles.hamburger} onClick={toggleMenu}>
+                <GiHamburgerMenu />
+            </button>
+            <nav className={`${styles.nav} ${isMenuOpen ? styles.navActive : ""}`}>
                 <ul className={styles.navItems}>
                     <li><a href="#">Início</a></li>
                     <li><a href="#">Sobre nós</a></li>
                     <li><a href="#">Processos</a></li>
                     <li><a href="#">Categorias</a></li>
-                    <li><a href="#">Principais Áreas</a></li>
-                    <li><a href="#">Cases de Sucesso</a></li>
+                    <li><a href="#">Timer</a></li>
                     <li><a href="#">Avaliações</a></li>
                     <li><a href="#">Vantagens</a></li>
                     <li><a href="#">Suporte</a></li>
+                    
+                <div className={styles.buttonsMobile}>
+                    <a href="https://hivelancers.vercel.app/Signup">
+                        <button className={styles.btnSignup}>
+                            Cadastrar <LuArrowUpRight />
+                        </button>
+                    </a>
+                    <a href="https://hivelancers.vercel.app/Login">
+                        <button className={styles.btnLogin}>Entrar</button>
+                    </a>
+                </div>
                 </ul>
+
+           
             </nav>
             <div className={styles.buttons}>
-                <a href="https://hivelancers.vercel.app/Login"><button className={styles.btnLogin}>Entrar</button></a>
-                <a href="https://hivelancers.vercel.app/Signup"><button className={styles.btnSignup}>Cadastrar <LuArrowUpRight /></button></a>
+                <a href="https://hivelancers.vercel.app/Login">
+                    <button className={styles.btnLogin}>Entrar</button>
+                </a>
+                <a href="https://hivelancers.vercel.app/Signup">
+                    <button className={styles.btnSignup}>
+                        Cadastrar <LuArrowUpRight />
+                    </button>
+                </a>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default Navbar;
